8a9a8815bd677225731707ec7006e34e2ba79e4c
Carl Larsson <130466691+Illuminy00@users.noreply.github.com>
2024-04-04 16:41:29 +0200

    hopefully fixed find obstacle
    hopefully fixed find obstacle

--- task_1/find_obs.m
+++ task_1/find_obs.m
@@ -6,16 +6,36 @@
 % This software is licensed under the MIT License
 % Refer to the LICENSE file for details
 %==========================================================================
-function x_obs = find_obs(rowidx, colidx, x)
+function x_obs = find_obs(sensor_matrix, x)
 
-distance_cell = {[-2;2],  [-1;2],  [0;2],  [1;2],  [2;2];
-                 [-2;1],  [-1;1],  [0;1],  [1;1],  [2;1];
-                 [-2;0],  [-1;0],  [0;0],  [1;0],  [2;0];
-                 [-2;-1], [-1;-1], [0;-1], [1;-1], [2;-1];
-                 [-2;-2], [-1;-2], [0;-2], [1;-2], [2;-2]};
+% Save all relative coordinates (to the robot) where obstacles are found
+obstacle_list = {};
 
-distance_matrix = reshape(distance_cell, 5, 5);
+% loop through entire matrix to search for obstacles
+for row = 1:5
+    for column = 1:5
+        % 0 is obstacle
+        if sensor_matrix(row,column) == 0
+            % Convert to relative coordinate where robot is in center, (3,3)
+            obstacle_list{end+1} = [row;column] - [3;3];
+        end
+    end
+end
 
-x_obs = x + distance_matrix{rowidx, colidx};
+% Take first element as best to begin with
+closest = 1;
+% Find closest obstacle (will always result in orthogonal)
+for obstacle = 1:numel(obstacle_list)
+    if norm(x-obstacle_list{obstacle}) < norm(x-obstacle_list{closest})
+        closest = obstacle;
+    end
+end
+
+if numel(obstacle_list) ~= 0
+    % Get global coordinate (obstacle list has relative to robot)
+    x_obs = x + obstacle_list{closest};
+else
+    x_obs = x + [3;3];
+end
 
 end
